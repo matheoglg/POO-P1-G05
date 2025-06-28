44fe1f511af3a5b9b34187f2e4223de1399e256e
matheoglg <mathiasgilergomez@gmail.com>
2025-06-28 12:31:49 -0500

    Create Hola.java

---
+++ Hola.java
@@ -0,0 +1,11 @@
+package espol.poo;
+
+import espol.poo.controlador.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ControladorApp controlador = new ControladorApp();
+        controlador.menuPrincipal();
+    }
+}
