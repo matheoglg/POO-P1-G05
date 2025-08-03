6e9f34361cedcf0866891d3c8d4c9bd912aedd71
matheoglg <mathiasgilergomez@gmail.com>
2025-08-03 17:30:46 -0500

    Creación de Pantalla de Clientes
    Creación inicial de layout de opción "Administrar Clientes" con un RecyclerView de la lista de clientes (por crear) y sus respectivos botones de "Agregar Cliente" y "Volver"

--- app/src/main/java/espol/poo/proyectopoog5/MainActivity.java
+++ app/src/main/java/espol/poo/proyectopoog5/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_principal);
         String file = "hola";
         String a = "eee";
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
