245a1c44c7f8745d5dfa1f39c504053a2f7edc19
ClydeVanAstrea <dylandvm10@gmail.com>
2025-08-12 20:08:20 -0500

    Ordenar lista de OrdenServicio desde la mas reciente por fecha
    se hace parse de string a LocalDate

--- app/src/main/java/espol/poo/proyectopoog5/activities/activitiesOrdenServicio/OrdenServicioActivity.java
+++ app/src/main/java/espol/poo/proyectopoog5/activities/activitiesOrdenServicio/OrdenServicioActivity.java
@@ -1,11 +1,15 @@
 package espol.poo.proyectopoog5.activities.activitiesOrdenServicio;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import espol.poo.proyectopoog5.R;
 import espol.poo.proyectopoog5.modelo.OrdenServicio;
@@ -37,6 +41,18 @@ public class OrdenServicioActivity extends AppCompatActivity {
         recyclerView1 = findViewById(R.id.rvOrdenServicio);
         recyclerView1.setLayoutManager(new LinearLayoutManager(this));
         listaOrdenes = OrdenServicio.cargarOrdenes(this);
+
+        // Ordenar por fecha más reciente primero
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            listaOrdenes.sort((o1, o2) -> {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                LocalDate fecha1 = LocalDate.parse(o1.getFecha(), formatter);
+                LocalDate fecha2 = LocalDate.parse(o2.getFecha(), formatter);
+                return fecha2.compareTo(fecha1); // invertir para el orden
+            });
+        }
+
+
         ordenAdapter = new OrdenServicioAdapter(listaOrdenes, this);
         recyclerView1.setAdapter(ordenAdapter);
     }
